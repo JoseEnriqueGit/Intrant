da1ad30c4fe6f4257049bb6c289b2c0a242ca48c
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2022-12-10 16:51:24 -0400

    citationSchema

---
+++ backend/src/models/CitationModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const citationSchema = new mongoose.Schema({
+	cedula: {
+		type: String,
+		trim: true,
+	},
+	correo: {
+		type: String,
+		trim: true,
+	},
+	telefono: {
+		type: String,
+		trim: true,
+	},
+	asunto: {
+		type: String,
+		trim: false,
+	},
+	oficina: {
+		type: String,
+		trim: false,
+	},
+	fecha: Date,
+	hora: {
+		type: String,
+		trim: false,
+	},
+});
+
+export default mongoose.model('Citation', citationSchema);
