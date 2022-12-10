054b62450903ee4a9888993a043ecad76a09d0d6
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2022-12-10 16:50:35 -0400

    Renamed

--- backend/src/routes/citation.route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-const { getCitation } = require("../controllers/citation.controller");
-
-router.route("/").get(getCitation);
-// router.route("/").post(postCitation);
-
-module.exports = router;
