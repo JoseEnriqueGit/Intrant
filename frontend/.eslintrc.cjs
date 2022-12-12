5c878da3a46a443a0310062861218c0ea2672c61
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2022-12-11 20:55:54 -0400

    Eslint and prettier

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+  settings: {
+		react: {
+			version: 'detect',
+		}
+	},
+	extends: [
+    'plugin:react/recommended',
+		'standard',
+		'plugin:react/jsx-runtime',
+		'eslint-config-prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'no-unused-vars': 'warn',
+		'react/prop-types': 'off',
+		'react/no-unknown-property': 'off',
+	},
+};
