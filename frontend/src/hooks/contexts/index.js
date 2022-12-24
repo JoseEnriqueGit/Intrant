0281ec30abc16507bb52053a23b4beceeeb86be9
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2022-12-24 18:58:00 -0400

    deleted:    frontend/src/hooks/contexts/DataInStorageContext.jsx

--- frontend/src/hooks/contexts/index.js
+++ frontend/src/hooks/contexts/index.js
@@ -1,4 +1,3 @@
 export { default as DataApiContext } from './DataApiContext';
-export { default as DataInStorageContext } from './DataInStorageContext';
 export { default as FormDataContext } from './FormDataContext';
 export { default as PageContext } from './PageContext';
