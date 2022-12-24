ec6b9c8bce0f644eee2e4544cfade670635c0073
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2022-12-24 18:55:28 -0400

    DataInStorageContextProvider Deleted

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,21 +5,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { FormDataContextProvider } from './hooks/contexts/FormDataContext';
 import { DataApiContextProvider } from './hooks/contexts/DataApiContext';
 import { PageContextProvider } from './hooks/contexts/PageContext';
-import { DataInStorageContextProvider } from './hooks/contexts/DataInStorageContext';
 import { UrlApiProvider } from './hooks/useEffects/useDataCedula';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<DataInStorageContextProvider>
-		<PageContextProvider>
-			<DataApiContextProvider>
-				<FormDataContextProvider>
-					<UrlApiProvider>
-						<BrowserRouter>
-							<App />
-						</BrowserRouter>
-					</UrlApiProvider>
-				</FormDataContextProvider>
-			</DataApiContextProvider>
-		</PageContextProvider>
-	</DataInStorageContextProvider>
+	<PageContextProvider>
+		<DataApiContextProvider>
+			<FormDataContextProvider>
+				<UrlApiProvider>
+					<BrowserRouter>
+						<App />
+					</BrowserRouter>
+				</UrlApiProvider>
+			</FormDataContextProvider>
+		</DataApiContextProvider>
+	</PageContextProvider>
 );
