9804553e3de61115fb929d28999309c9baacd9f8
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2022-12-27 17:33:41 -0400

    Refactoring delete fuction.

--- frontend/src/api/deleteCitation.js
+++ frontend/src/api/deleteCitation.js
@@ -2,14 +2,12 @@
 import axios from 'axios';
 
 export const deleteCitation = async cedula => {
-	axios
-		.delete('https://intrant-api.onrender.com/delete-citation/' + cedula)
-		.then(res => {
-			if (res.status === 200 && res.data.result.deletedCount === 1) {
-				return true;
-			} else {
-				return false;
-			}
-		})
-		.catch(error => console.log(error));
+	const res = await axios.delete('https://intrant-api.onrender.com/delete-citation/' + cedula);
+	// return res
+	if(res.status === 200 && res.data.result.deletedCount === 1) {
+		return true
+	}
+	else{
+		return false
+	}
 };
