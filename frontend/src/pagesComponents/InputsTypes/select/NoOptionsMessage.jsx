3ff606e00133dd391d12b0f3a8f7352b0bb97d0e
Jose Enrique <enriquemk51@gmail.com>
2023-01-10 22:08:18 -0400

    Change NoOptionsMessage.

--- frontend/src/pagesComponents/InputsTypes/select/NoOptionsMessage.jsx
+++ frontend/src/pagesComponents/InputsTypes/select/NoOptionsMessage.jsx
@@ -3,7 +3,7 @@ import { components } from 'react-select';
 export const NoOptionsMessage = props => {
     return (
       <components.NoOptionsMessage {...props}>
-        <span className="custom-css-class">Seleccione una de nuestas oficinas</span> 
+        <span className="custom-css-class">Seleccione uno de nuestros servicios</span> 
       </components.NoOptionsMessage>
     );
   };
